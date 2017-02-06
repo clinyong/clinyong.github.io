385460d3456313f61904d0daaecb06beab72f10a
clinyong <clinyong@gmail.com>
2017-02-06 23:44:28 +0800

    support date

--- build.js
+++ build.js
@@ -3,9 +3,33 @@ const fs = require('fs');
 const showdown = require('showdown'),
     converter = new showdown.Converter();
 
+function extractMetaData(text) {
+    const lines = text.split("\n")
+
+    let i = 0;
+    for (; i < lines.length; i++) {
+        if (lines[i] === '---') {
+            break;
+        }
+    }
+
+    const meta = {}
+    lines.slice(0, i).forEach(line => {
+        const items = line.split(":")
+        meta[items[0]] = items[1].trim()
+    })
+
+    const content = lines.slice(i + 1).join('\n');
+    return {
+        meta,
+        content
+    }
+}
 
 const text = fs.readFileSync('./src/1.md', 'utf8');
-const body = converter.makeHtml(text);
+const result = extractMetaData(text);
+const {title, date} = result.meta;
+const body = converter.makeHtml(result.content);
 const html = `
 <!DOCTYPE html>
 <html lang="zh">
@@ -24,7 +48,11 @@ const html = `
             </div>
         </header>
         <div class="content">
+            <h1 class="title">${title}</h1>
             ${body}
+            <div class="date">
+                ${result.meta.date}
+            </div>
         </div>
     </div>
 </body>
