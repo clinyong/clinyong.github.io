cac3b4e18e782d369dff1687f2d6595f72ad3755
clinyong <clinyong@gmail.com>
2017-03-18 16:01:55 +0800

    publish

--- 5x5/sw.js
+++ 5x5/sw.js
@@ -1,4 +1,4 @@
-var serviceWorkerOption = {"assets":["/js/vendor.f0aa9b9827fc9b9eb883.js","/js/app.c79bbcba8a95f0402c37.js","/js/manifest.887a95c0f1ab6c1ba32b.js","/index.html","/image/144.png","/image/96.png","/manifest.json","/image/192.png"]};
+var serviceWorkerOption = {"assets":["/js/vendor.f0aa9b9827fc9b9eb883.js","/js/app.c79bbcba8a95f0402c37.js","/js/manifest.887a95c0f1ab6c1ba32b.js","/index.html","/image/144.png","/manifest.json","/image/192.png","/image/96.png"]};
         
         /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
